89e522dfe722ac377dd5299cae4a4b2f5ce0bea0
Vincent Otieno <vinnyotieno004@gmail.com>
2024-11-07 02:28:29 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  build: {
+    outDir: 'dist'  // Ensures the output directory is `dist`
+  }
 });
