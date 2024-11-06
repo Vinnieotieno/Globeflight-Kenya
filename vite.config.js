13bfdf8f827668b828852f3c85863a0fc26583f3
Vincent Otieno <vinnyotieno004@gmail.com>
2024-11-07 02:41:01 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 // vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'  // Ensures the output directory is `dist`
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Set alias for "@" to the "src" directory
+    },
+  },
 });
+
