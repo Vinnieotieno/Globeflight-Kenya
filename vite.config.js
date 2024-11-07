796c220ecbba4a56a40e856605aacbb603c3c023
Vincent Otieno <vinnyotieno004@gmail.com>
2024-11-07 22:52:12 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // Alias '@' to the 'src' directory
+      '@': path.resolve(__dirname, './src'),  // Correct alias path for '@'
     },
   },
 });
+
