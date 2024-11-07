732f7729572e709f1dac6917fef7ad7a4f3c2e93
Vincent Otieno <vinnyotieno004@gmail.com>
2024-11-07 04:36:16 +0300

    blog

--- src/pages/Blog/sections/BlogDetail.jsx
+++ src/pages/Blog/sections/BlogDetail.jsx
@@ -6,7 +6,7 @@ import axios from "axios"
 import Hero from "@/pages/Blog/sections/Hero";
 import CallToActionSection from "@/components/CallToActionSection"
 import LearnMore from "@/pages/Blog/sections/LearnMore";
-import ScrollOnSideSection from "../../components/ScrollOnSideSection"
+import ScrollOnSideSection from "@/components/ScrollOnSideSection";
 import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Card, CardContent } from "@/components/ui/card"
